4aaa2f9306deb1a6efa7703e7a3c35721aa3813f
Neven Bedr <neven2225@gmail.com>
2020-07-09 17:42:18 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Add your functions here
+function map(src, func) {
+  return src.map(func)
+}
+
+function reduce(src, func, param) {
+  if(!!param){
+    return src.reduce(func, param)
+  }else {
+    return src.reduce(func)
+  }
+
+}
